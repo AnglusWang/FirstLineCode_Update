d75138281b0550e8e051ca227b840cee582545b3
AnglusWang <AnglusWang@163.com>
2016-07-11 22:21:55 +0800

    do some update

--- ChapterThird/UIWidgetTest/app/src/main/java/com/angluswang/uiwidgettest/MainActivity.java
+++ ChapterThird/UIWidgetTest/app/src/main/java/com/angluswang/uiwidgettest/MainActivity.java
@@ -1,13 +1,31 @@
 package com.angluswang.uiwidgettest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private Button btn;
+    private EditText etText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.btn_button);
+        etText = (EditText) findViewById(R.id.et_text);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String inputText = etText.getText().toString();
+                Toast.makeText(MainActivity.this, "EditText 文本框内容为： " + inputText,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
