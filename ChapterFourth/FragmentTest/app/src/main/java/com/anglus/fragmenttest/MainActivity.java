6a68e44d7f63dd0355eaa337c6e37d8894faf468
AnglusWang <angluswang@163.com>
2016-10-09 21:13:30 +0800

    4.2.2 动态添加碎片

--- ChapterFourth/FragmentTest/app/src/main/java/com/anglus/fragmenttest/MainActivity.java
+++ ChapterFourth/FragmentTest/app/src/main/java/com/anglus/fragmenttest/MainActivity.java
@@ -1,13 +1,36 @@
 package com.anglus.fragmenttest;
 
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity
+        implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+                AnotherRightFragment fragment = new AnotherRightFragment();
+                FragmentManager fm = getFragmentManager();
+                FragmentTransaction transaction = fm.beginTransaction();
+                transaction.replace(R.id.right_layout, fragment);
+                transaction.commit();
+                break;
+            default:
+                break;
+        }
     }
 }
