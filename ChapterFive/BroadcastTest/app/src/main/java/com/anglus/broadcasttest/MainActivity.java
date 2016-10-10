5e9044c32e73e2d30dfd198f45611a58533a4418
AnglusWang <angluswang@163.com>
2016-10-10 09:46:40 +0800

    5.2 接收系统广播 --- 动态注册监听网络变化

---
+++ ChapterFive/BroadcastTest/app/src/main/java/com/anglus/broadcasttest/MainActivity.java
@@ -0,0 +1,46 @@
+package com.anglus.broadcasttest;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    private IntentFilter mIntentFilter;
+    private NetworkChangerReceiver mNetworkChangerReceiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mIntentFilter = new IntentFilter();
+        mIntentFilter.addAction("android.net.com.CONNECTIVITY_CHANGE");
+        mNetworkChangerReceiver = new NetworkChangerReceiver();
+        registerReceiver(mNetworkChangerReceiver, mIntentFilter);
+    }
+
+    private class NetworkChangerReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            ConnectivityManager connectivityManager =
+                    (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+
+            if (networkInfo != null && networkInfo.isAvailable()) {
+                Toast.makeText(MainActivity.this, "network is available",
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "network is unavailable",
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
