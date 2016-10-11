cd767c4e11d6380acca9dab16fe662840cfd335e
AnglusWang <angluswang@163.com>
2016-10-11 11:06:33 +0800

    从 SharedPreferences 中读取数据

--- ChapterSix/SharedPreferencesTest/app/src/main/java/com/anglus/sharedpreferencestest/MainActivity.java
+++ ChapterSix/SharedPreferencesTest/app/src/main/java/com/anglus/sharedpreferencestest/MainActivity.java
@@ -3,12 +3,14 @@ package com.anglus.sharedpreferencestest;
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends Activity {
 
     private Button btn_saveData;
+    private Button restoreData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +18,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         btn_saveData = (Button) findViewById(R.id.btn_saveData);
+        restoreData = (Button) findViewById(R.id.restore_data);
         btn_saveData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -25,6 +28,18 @@ public class MainActivity extends Activity {
                 editor.putInt("age", 28);
                 editor.putBoolean("married", false);
                 editor.commit();
+            }
+        });
+        restoreData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences pref = getSharedPreferences("data", MODE_PRIVATE);
+                String name = pref.getString("name", "");
+                int age = pref.getInt("age", 0);
+                boolean married = pref.getBoolean("married", false);
+                Log.d("MainActivity", "name is " + name);
+                Log.d("MainActivity", "age is " + age);
+                Log.d("MainActivity", "married is " + married);
             }
         });
     }
