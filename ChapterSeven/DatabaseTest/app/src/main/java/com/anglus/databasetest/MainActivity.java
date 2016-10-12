b2ad16948ba2d2f76fe08935e65029190a884046
AnglusWang <angluswang@163.com>
2016-10-12 22:08:39 +0800

    实现跨程序数据共享 --- 内容提供者

---
+++ ChapterSeven/DatabaseTest/app/src/main/java/com/anglus/databasetest/MainActivity.java
@@ -0,0 +1,129 @@
+package com.anglus.databasetest;
+
+import android.app.Activity;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    private static final String TAG = "MainActivity";
+
+    private MyDatabaseHelper dbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        dbHelper = new MyDatabaseHelper(this, "BookStore.db", null, 2);
+        Button createDatabase = (Button) findViewById(R.id.create_database);
+        createDatabase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dbHelper.getWritableDatabase();
+            }
+        });
+
+        Button addData = (Button) findViewById(R.id.add_data);
+        addData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                // 使用 ContentValues 对需要添加的数据进行组装
+                ContentValues values = new ContentValues();
+
+                // 开始组装第一条数据
+                values.put("name", "The Da Vin Code");
+                values.put("author", "Dan Brown");
+                values.put("pages", 454);
+                values.put("price", 16.96);
+                db.insert("Book", null, values);// 插入第一条数据
+                values.clear();
+
+                // 开始组装第二条数据
+                values.put("name", "The Lost Symbol");
+                values.put("author", "Dan Brown");
+                values.put("pages", 510);
+                values.put("price", 19.95);
+                db.insert("Book", null, values);// 插入第二条数据
+            }
+        });
+
+        Button updateDate = (Button) findViewById(R.id.update_data);
+        updateDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                values.put("price", 10.99);
+                db.update("Book", values, "name=?", new String[]{"The Da Vin Code"});
+            }
+        });
+
+        Button delete_data = (Button) findViewById(R.id.delete_data);
+        delete_data.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                db.delete("Book", "pages > ?", new String[]{"500"});
+            }
+        });
+
+        Button query_data = (Button) findViewById(R.id.query_data);
+        query_data.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                // 查询 Book 表中所有的数据
+                Cursor cursor = db.query("Book", null, null, null, null, null, null);
+                if (cursor.moveToFirst()) {
+                    do {
+                        // 遍历 Cursor 对象，取出数据并打印
+                        String name = cursor.getString(cursor.getColumnIndex("name"));
+                        String author = cursor.getString(cursor.getColumnIndex("author"));
+                        int pages = cursor.getInt(cursor.getColumnIndex("pages"));
+                        double price = cursor.getDouble(cursor.getColumnIndex("price"));
+
+                        Log.d(TAG, "book name is " + name);
+                        Log.d(TAG, "book author is " + author);
+                        Log.d(TAG, "book pages is " + pages);
+                        Log.d(TAG, "book price is " + price);
+                    } while (cursor.moveToNext());
+                }
+                cursor.close();
+            }
+        });
+
+        Button replaceData = (Button) findViewById(R.id.replace_data);
+        replaceData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                db.beginTransaction();// 开启事务
+                try {
+                    db.delete("Book", null, null);
+                    if (true) {
+                        // 在这里手动抛出一个异常，让事务失败
+//                        throw new NullPointerException();
+                    }
+                    ContentValues values = new ContentValues();
+                    values.put("name", "Game of Thrones");
+                    values.put("author", "George Martin");
+                    values.put("pages", 720);
+                    values.put("price", 20.85);
+                    db.insert("Book", null, values);
+                    db.setTransactionSuccessful();// 事务已经执行成功
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    db.endTransaction();// 结束事务
+                }
+            }
+        });
+    }
+}
