9b5b562474fc1c5fe2485a87c18e5ad5d549034c
AnglusWang <angluswang@163.com>
2016-10-12 11:15:03 +0800

    添加数据

--- ChapterSix/DatabaseTest/app/src/main/java/com/anglus/databasetest/MainActivity.java
+++ ChapterSix/DatabaseTest/app/src/main/java/com/anglus/databasetest/MainActivity.java
@@ -1,6 +1,8 @@
 package com.anglus.databasetest;
 
 import android.app.Activity;
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,6 +22,31 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
                 dbHelper.getWritableDatabase();
+            }
+        });
+
+        Button addData = (Button) findViewById(R.id.add_data);
+        addData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                // 使用 ContentValues 对需要添加的数据进行组装
+                ContentValues values = new ContentValues();
+
+                // 开始组装第一条数据
+                values.put("name", "The Da Vin Code");
+                values.put("author", "Dan Brown");
+                values.put("pages", 454);
+                values.put("price", 16.96);
+                db.insert("Book", null, values);// 插入第一条数据
+                values.clear();
+
+                // 开始组装第二条数据
+                values.put("name", "The Lost Symbol");
+                values.put("author", "Dan Brown");
+                values.put("pages", 510);
+                values.put("price", 19.95);
+                db.insert("Book", null, values);// 插入第二条数据
             }
         });
     }
