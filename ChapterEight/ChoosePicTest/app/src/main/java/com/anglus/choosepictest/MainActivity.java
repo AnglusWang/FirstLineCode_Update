d62bce4c9726708b141d60597658e443f35ebdd0
AnglusWang <angluswang@163.com>
2016-10-21 11:17:31 +0800

    8.3.3 从相册中选择照片 --- 书中代码需完善（使用 米3 测试，发现加载图片出错）

--- ChapterEight/ChoosePicTest/app/src/main/java/com/anglus/choosepictest/MainActivity.java
+++ ChapterEight/ChoosePicTest/app/src/main/java/com/anglus/choosepictest/MainActivity.java
@@ -1,27 +1,37 @@
 package com.anglus.choosepictest;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.ContentUris;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import static android.content.ContentValues.TAG;
+
 public class MainActivity extends Activity {
 
     public static final int TAKE_PHOTO = 1;// 拍照
     public static final int CROP_PHOTO = 2;// 剪切
+    public static final int CHOOSE_PHOTO = 3;// 选择
 
-    private Button takePhoto;
+    private Button takePhoto, chooseFromAlbum;
     private ImageView picture;
     private Uri imageUri;
 
@@ -35,6 +45,7 @@ public class MainActivity extends Activity {
 
     private void initView() {
         takePhoto = (Button) findViewById(R.id.take_photo);
+        chooseFromAlbum = (Button) findViewById(R.id.choose_from_album);
         picture = (ImageView) findViewById(R.id.picture);
 
         takePhoto.setOnClickListener(new View.OnClickListener() {
@@ -55,6 +66,15 @@ public class MainActivity extends Activity {
                 Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
                 intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
                 startActivityForResult(intent, TAKE_PHOTO);// 启动相机程序
+            }
+        });
+
+        chooseFromAlbum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("android.intent.action.GET_CONTENT");
+                intent.setType("image/*");
+                startActivityForResult(intent, CHOOSE_PHOTO);// 打开相册
             }
         });
     }
@@ -82,8 +102,84 @@ public class MainActivity extends Activity {
                     }
                 }
                 break;
+            case CHOOSE_PHOTO:
+                if (resultCode == RESULT_OK) {
+                    // 判断手机系统版本号
+                    if (Build.VERSION.SDK_INT >= 19) {
+                        // 4.4 及以上系统调用此方法
+                        handleImageOnKitKat(data);
+                    } else {
+                        handleImageBeforeKitKat(data);
+                    }
+                }
+                break;
             default:
                 break;
+        }
+    }
+
+    @TargetApi(19)
+    private void handleImageOnKitKat(Intent data) {
+        String imagePath = null;
+        Uri uri = data.getData();
+        Log.d(TAG, "handleImageOnKitKat: uri -> " + uri);
+        if (DocumentsContract.isDocumentUri(this, uri)) {
+            // 如果是 document 类型的 Uri，则通过 document id 处理
+            String docId = DocumentsContract.getDocumentId(uri);
+            if ("com.android.providers.media.documents"
+                    .equals(uri.getAuthority())) {
+                String id = docId.split(":")[1];// 解析出数字格式的 id
+                String selection = MediaStore.Images.Media._ID + "=" + id;
+                imagePath = getImagePath(
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI, selection);
+            } else if ("com.android.providers.downloads.documents"
+                    .equals(uri.getAuthority())) {
+                Uri contentUri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"),
+                        Long.valueOf(docId));
+                imagePath = getImagePath(contentUri, null);
+            }
+        } else if ("content".equalsIgnoreCase(uri.getScheme())) {
+            // 如果不是 document 类型的 Uri， 则使用普通方式处理
+            imagePath = getImagePath(uri, null);
+        }
+        displayImage(imagePath);// 根据图片路径显示图片
+    }
+
+    private void handleImageBeforeKitKat(Intent data) {
+        Uri uri = data.getData();
+        String imagePath = getImagePath(uri, null);
+        displayImage(imagePath);
+    }
+
+    /**
+     * 通过 Uri 和 selection 来获取真实的图片路径
+     *
+     * @param externalContentUri
+     * @param selection
+     * @return
+     */
+    private String getImagePath(Uri externalContentUri, String selection) {
+        String path = null;
+        // 通过 Uri 和 selection 来获取真实的图片路径
+        Cursor cursor = getContentResolver().query(externalContentUri, null,
+                selection, null, null);
+        if (cursor != null) {
+            if (cursor.moveToNext()) {
+                path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+            }
+            cursor.close();
+        }
+        return path;
+    }
+
+    private void displayImage(String imagePath) {
+        if (imagePath != null) {
+            Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
+            picture.setImageBitmap(bitmap);
+        } else {
+            Toast.makeText(this, "failed to get Image",
+                    Toast.LENGTH_SHORT).show();
         }
     }
 }
